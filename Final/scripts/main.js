864fda69086334c5c39cbaec9dba7d843ef2e91e
Leo Zhong <zzhong@byupathway.edu>
2025-06-17 00:46:21 -0700

    Remove old folder Final

--- Final/scripts/main.js
+++
@@ -1,7 +0,0 @@
-// Responsive Menu Toggle
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
